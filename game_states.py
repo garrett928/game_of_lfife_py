1711595c313be1cf424f140e712255d15caa5920
garrett928 <garrett7621@gmail.com>
2019-02-20 23:57:26 -0500

    adds list to keep track of cells when dragging

--- game_states.py
+++ game_states.py
@@ -1,5 +1,5 @@
 is_dragging = False
-
+cell_dragging_list = []
 
 def get_mouse_dragging():
     return is_dragging
@@ -8,3 +8,5 @@ def get_mouse_dragging():
 def set_mouse_dragging(dragging):
     global is_dragging
     is_dragging = dragging
+    if not is_dragging:
+        cell_dragging_list.clear()
